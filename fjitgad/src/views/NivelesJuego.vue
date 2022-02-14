85c42d1a30af7afbb1add2ce05229df7fd14e74d
josegerar <45176268+josegerar@users.noreply.github.com>
2022-02-14 15:41:33 -0500

    SDFDF

---
+++ fjitgad/src/views/NivelesJuego.vue
@@ -0,0 +1,19 @@
+<template>
+  <main-layout-juego :backgroundBlanc="true">
+    <my-loading v-if="Loading" />
+    <div v-else class="container" style="margin-top: 10vh">
+      <encabezado-juego :routes="routes" />
+      <template v-for="(tipojuego, i) in JuegosPorActividad" :key="i">
+      </template>
+    </div>
+  </main-layout-juego>
+</template>
+
+<script>
+export default {
+    name: "NivelesJuego",
+    setup(props, context) {
+        
+    },
+}
+</script>
