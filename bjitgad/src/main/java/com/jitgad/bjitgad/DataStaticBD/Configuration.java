4c621d30089fc27842765b9d2d8c18fee9fec955
Faroukmn97 <29679158+Faroukmn97@users.noreply.github.com>
2022-03-15 19:08:58 -0500

    push

--- bjitgad/src/main/java/com/jitgad/bjitgad/DataStaticBD/Configuration.java
+++ bjitgad/src/main/java/com/jitgad/bjitgad/DataStaticBD/Configuration.java
@@ -11,7 +11,8 @@ public class Configuration {
     public static String DATABASEPASSWORD = "e2519d7aa881ecf70a6228c203e777fd403925bac01379cb5a95a1ee44883f78";
     public static String DATABASEURL = "jdbc:postgresql://ec2-34-195-69-118.compute-1.amazonaws.com:5432/dfc5nss9nj0c2e";
     public static String JWTKEY = "12345GFEDCBA$.$123Barcelona";
-    public static String IPDOMINIOSERVIDOR= "http://192.241.152.203:8080/bjitgad/";
+    //public static String IPDOMINIOSERVIDOR= "http://192.241.152.203:8080/bjitgad/";
+    public static String IPDOMINIOSERVIDOR= "http://fyc.uteq.edu.ec:8080/bjitgad/";
     public static boolean DEBUG = false;
     
     
